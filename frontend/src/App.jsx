6c04f1fca4bdec4d855c5d4109fc69a55dd559ba
Kisan-jena <kisan.k.k.jena.01@gmail.com>
2025-07-21 11:39:09 +0530

    fe set-up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <div className='bg-gray-500 text-2xl'>
+        kisan
+      </div>
+    </div>
+  )
+}
+
+export default App
